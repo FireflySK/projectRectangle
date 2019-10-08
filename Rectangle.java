48e09d99f18e934b9036e14ab5cce13480bd701b
Damian Matysko <damian.matysko@akademiasovy.sk>
2019-10-08 11:19:12 +0200

    **

--- Rectangle.java
+++ Rectangle.java
@@ -31,4 +31,42 @@ public class Rectangle {
             System.out.println();
         }
     }
+
+    public void roots(){
+        Scanner myObj = new Scanner(System.in);
+        double D=0;
+        double x=0;
+        double x1=0;
+        double x2=0;
+        double a=0;
+        double b=0;
+        double c=0;
+        System.out.println("Hello Im a program to solve quadratic equation");
+        System.out.println("Enter a:");
+        a = myObj.nextInt();
+        System.out.println("Enter b:");
+        b = myObj.nextInt();
+        System.out.println("Enter c:");
+        c = myObj.nextInt();
+
+        if (a==0){
+            System.out.println("Nieje to qvadraticka rovnica");
+        }else{
+            D= Math.pow(b,2)-4*a*c;
+            System.out.println("D=" + D);
+            if (D>0){
+                x1=(((-b)+Math.sqrt(D))/2*a);
+                x2=(((-b)-Math.sqrt(D))/2*a);
+                System.out.println("dve riesenia: x1=" + x1 + " x2=" + x2 );
+            }
+            if (D==0){
+                x1=(-b)/(2*a);
+                System.out.println("Jedno riesenie: x=" + x );
+            }
+            if (D<0){
+                System.out.println("Nema riesenie");
+            }
+        }
+    }
+
 }
